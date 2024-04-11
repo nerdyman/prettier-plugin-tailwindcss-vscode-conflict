c1f3688ab93eab5057205b1ae15af431f0e8a768
nerdyman <averynerdyman@gmail.com>
2024-04-11 12:10:24 +0100

    working repro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,14 @@ const config = {
   ],
   theme: {
     extend: {
+      colors: {
+        example: {
+          100: "#00F000",
+          200: "#00FFFF",
+          300: "#F0000F",
+        },
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
